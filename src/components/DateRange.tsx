dc7017ead60f047a2602ccb8438c787cd8995524
Matheus Gomes <maatheus.gsr@gmail.com>
2023-08-16 16:14:49 -0300

    fix: fix context variable names

--- src/components/DateRange.tsx
+++ src/components/DateRange.tsx
@@ -3,19 +3,19 @@ import DateInput from "./DateInput";
 import { useData } from "../context/DataContext";
 
 const DateRange = () => {
-  const { inicio, setInicio, final, setFinal } = useData();
+  const { start, setStart, end, setEnd } = useData();
 
   return (
     <form className="box flex" onSubmit={e => e.preventDefault()}>
       <DateInput
         label="Início"
-        value={inicio}
-        onChange={({ target }) => setInicio(target.value)}
+        value={start}
+        onChange={({ target }) => setStart(target.value)}
       />
       <DateInput
         label="Final"
-        value={final}
-        onChange={({ target }) => setFinal(target.value)}
+        value={end}
+        onChange={({ target }) => setEnd(target.value)}
       />
     </form>
   );
