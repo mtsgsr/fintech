fad8e6cc6b3e88bc784936f6deee8a0a302162ed
Matheus Gomes <maatheus.gsr@gmail.com>
2023-08-16 16:57:08 -0300

    feat: add data context provider, components and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./style.css";
 
+import { DataContextProvider } from "./context/DataContext";
+
+import Sidenav from "./components/Sidenav";
+import Header from "./components/Header";
+
+import Summary from "./pages/Summary";
+import Sales from "./pages/Sales";
+
 function App() {
-  return <></>;
+  return (
+    <DataContextProvider>
+      <div className="container">
+        <Sidenav />
+        <main>
+          <Header />
+          <Summary />
+          <Sales />
+        </main>
+      </div>
+    </DataContextProvider>
+  );
 }
 
 export default App;
