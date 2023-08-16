967bf45fe29833d960705cba231540d930ede350
Matheus Gomes <maatheus.gsr@gmail.com>
2023-08-16 16:16:47 -0300

    feat: add summary page

---
+++ src/pages/Summary.tsx
@@ -0,0 +1,43 @@
+import { useData } from "../context/DataContext";
+
+const Summary = () => {
+  const { data } = useData();
+
+  if (data === null) return null;
+  return (
+    <section>
+      <div className="summary flex mb">
+        <div className="box">
+          <h2>Vendas</h2>
+          <span>
+            {data
+              .filter(item => item.status !== "falha")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+        <div className="box">
+          <h2>Recebido</h2>
+          <span>
+            {data
+              .filter(item => item.status === "pago")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+        <div className="box">
+          <h2>Processando</h2>
+          <span>
+            {data
+              .filter(item => item.status === "processando")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+      </div>
+      <div className="box mb">Gráficos</div>
+    </section>
+  );
+};
+
+export default Summary;
