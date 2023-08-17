a3d881ea9f0489ca75a981a76b3f65b68b17aa5a
Matheus Gomes <maatheus.gsr@gmail.com>
2023-08-17 16:29:09 -0300

    feat: add dynamic page title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,36 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+
 import DateRange from "./DateRange";
 import Months from "./Months";
 
 const Header = () => {
+  const [title, setTitle] = React.useState("Resumo");
+  const location = useLocation();
+
+  React.useEffect(() => {
+    let id;
+    if (location.pathname.startsWith("/sales/")) id = location.pathname;
+    switch (location.pathname) {
+      case "/sales":
+        setTitle("Vendas");
+        document.title = "Fintech | Vendas";
+        break;
+      case id:
+        setTitle("Venda");
+        document.title = "Fintech | Venda";
+        break;
+      default:
+        setTitle("Resumo");
+        document.title = "Fintech | Resumo";
+    }
+  }, [location]);
+
   return (
     <header className="mb">
-      <div className="mb">
+      <div className="daterange mb">
         <DateRange />
+        <h1 className="box bg-3">{title}</h1>
       </div>
       <Months />
     </header>
